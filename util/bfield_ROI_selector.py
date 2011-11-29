95ef6fa3d898cf77a4b7c986b2ee609b5e26db45
Sam <sam.lindenrathen@gmail.com>
2011-11-29 15:29:27 +0000

    working bfield_ROI_selector

--- util/bfield_ROI_selector.py
+++ util/bfield_ROI_selector.py
@@ -25,37 +25,51 @@ def main():
                 first_line = False # first line is one we want to re-write
                 continue
             elif line_count < args.ignore_lines:
+                continue
                 tmp_file.write(line)
-            else:
-                x,y,z = line.split()[:3]
-                if not isInROI(roi, (x,y,z)): 
+            else:  
+                position = line.split()[:3]
+                position = [float(i) for i in position]
+                if not isInROI(roi, position): 
+                    # print 'BAD LINE line', line, 'xyz', x, y, z
                     continue
                 else:
+                    # print 'GOOD LINE line', line, 'xyz', x, y, z
                     tmp_file.write(line)
                     # need to know how many values on each axis
-                    if (not x in x_vals): x_vals.append(x)
-                    if (not y in y_vals): y_vals.append(y)
-                    if (not z in z_vals): z_vals.append(z)
+                    if (not position[0] in x_vals): x_vals.append(position[0])
+                    if (not position[1] in y_vals): y_vals.append(position[1])
+                    if (not position[2] in z_vals): z_vals.append(position[2])
     
     tmp_file.seek(0)
     
     with open(args.outfilename, 'w') as out_file:
         out_file.write("%i %i %i\n"%(len(x_vals), len(y_vals), len(z_vals)))
-        
         for line in tmp_file:
             out_file.write(line)
-    
-    tmp_file.close()            
+        tmp_file.close()            
+    print x_vals
+    print y_vals
+    print z_vals
+    print "stats"
+    print " |   min   |   max   |   average "
+    print "x| %7i | %7i | %5.1f"%minMaxAver(x_vals)
+    print "y| %7i | %7i | %5.1f"%minMaxAver(y_vals)
+    print "z| %7i | %7i | %5.1f"%minMaxAver(z_vals)
+
+def minMaxAver(vals):
+    return min(vals), max(vals), sum(vals)/len(vals)
 
 
 def isInROI(roi, position):
     """tests that the position is within the region of interest"""
     pos = dict(zip(('x','y','z'), position)) 
     for i in roi: # roi has indexs (x,y,z)
-        # roi[i][0] == min; roi[i][1] = max
-        inROI = (roi[i][0] < pos[i] < roi[i][1]) or \
-                ((roi[i][0] < pos[i]) and (roi[i][1]==None)) or \
-                ((roi[i][0] == None)  and (pos[i] < roi[i][1]))
+        min, max = roi[i][0], roi[i][1]
+        
+        inROI = ( min < pos[i] < max) or \
+                ((min < pos[i]) and (max==None)) or \
+                ((min == None ) and (pos[i] < max))
                 # test between min and max, or >/< min/max if other is "None"
         if not inROI: return False
     return True
@@ -80,7 +94,7 @@ def getArgs():
     parser.add_argument('--zdown', dest='zmin', 
                         default=None, type=float, help='min z-val')
     
-    parser.add_argument('--ignore', dest='ignore_lines', default=9, 
+    parser.add_argument('--ignore', dest='ignore_lines', default=10, 
                         type=int, help='number of lines not to test')
     
     
@@ -95,43 +109,25 @@ def getArgs():
     if args.xmax == None and args.xmin == None and \
             args.ymax == None and args.ymin == None and \
             args.zmax == None and args.zmin == None:
-       args.xmax, args.xmin = 399, 1401
-       args.ymax, args.ymin = -1,  1101
-       args.zmax, args.zmin = 3099, -4101
+       args.xmin, args.xmax = 399,  1401
+       args.ymin, args.ymax = -1,   1001
+       args.zmin, args.zmax = 3099, 4101
        # default ROI is 1x1x1m centred on (900mm, 0mm, 3600mm)
        
     return args
 
 
 def testRoi():
-    roi = {'x':(-10, 10),'y':(-10, 10),'z':(-10, 10),}
-    f = lambda l: (l[1],l[2],l[0]) # rotate left function
-    posF = (-11, 0, 0) # position that will return false 
-    posF2 = ( 11, 0, 0)
-    tests = [(0,0,0),(-11,-11,-11),(11,11,11)] 
-    roi2 = {'x':(-10, None),'y':(-10, None),'z':(-10, None),}
-    roi3 = {'x':(None, 10),'y':(None, 10),'z':(None, 10),}
-    for i in range(3): # generate tests for all dimensions
-        tests.append(posF)
-        tests.append(posF2)
-        posF=f(posF)
-        posF2=f(posF2)
-        
-    print "tests", tests
-    print "roi1", roi
-    print "roi2", roi2
-    print "roi3", roi3, "\n\n"
+    roi = {'x':(10, 20), 'y':(10, 20), 'z':(10, 20)}
+    in_roi = ((11, 12, 13), (11, 11, 11), (19, 19, 19))
+    not_in_roi = ((11,11,30), (11, 30, 11), (30,11,11)) 
+    print "in ROI"
+    for i in in_roi: print isInROI(roi, i)
+    print "not in ROI"
+    for i in not_in_roi: print isInROI(roi, i)
     
-    for test in tests: 
-        print "*"*40
-        print "test" , test
-        print "roi1", roi  
-        print isInROI(roi,  test)
-        print "roi2", roi2 
-        print isInROI(roi2, test)
-        print "roi3", roi3 
-        print isInROI(roi3, test)
                
+
 
 if __name__ == '__main__':
     main()

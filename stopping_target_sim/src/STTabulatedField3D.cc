0e1632180a55ffd67ead204965cbd6bdc2c561de
Sam <sam.lindenrathen@gmail.com>
2011-12-06 10:50:31 +0000

    missed merge

--- stopping_target_sim/src/STTabulatedField3D.cc
+++ stopping_target_sim/src/STTabulatedField3D.cc
@@ -462,9 +462,3 @@ void STTabulatedField3D::update()
     ClearField();
     Init();
 }
-=======
-void STTabulatedField3D::GetField(const char* filename)
-{
-    // as getField() but output is to a file instead
-}
->>>>>>> 507da4650ea93777f609c8b9d13c9b1b05d26444

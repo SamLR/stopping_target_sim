dccec574cf4b102cdc6844c0f2ad2e99f08c6fc6
Sam LR <sam.lindenrathen@gmail.com>
2012-01-30 16:59:48 +0000

    added monitor class

---
+++ stopping_target_sim/include/STMonitor.hh
@@ -0,0 +1,14 @@
+//
+//  STMonitor.hh
+//  
+//
+//  Created by Sam Cook on 30/01/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef _STMonitor_hh
+#define _STMonitor_hh
+
+
+
+#endif
---
+++ stopping_target_sim/src/STMonitor.cc
@@ -0,0 +1,9 @@
+//
+//  STMonitor.cc
+//  
+//
+//  Created by Sam Cook on 30/01/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
